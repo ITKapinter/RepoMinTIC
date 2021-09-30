85ed4a7376016f3d5f26ad1d361a0046243a026f
mreyesq <marcereyesq@gmail.com>
2021-09-29 22:09:57 -0500

    Agregar ajustes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,14 +4,14 @@ import logo from 'media/logo.png';
 
 const Navbar = () => {
   return (
-    <nav className='bg-gray-300'>
+    <nav className='bg-purple-300'>
       <ul className='flex w-full justify-between my-3'>
         <li> <img className='h-12 m-3 ' src={logo} alt="imagen" /></li>
         
         <li className='px-3'>
         {/* Link: para q al dar click en el boton me redirija a la pág de login */}
           <Link to='/login'> 
-            <button className='bg-indigo-500 p-2 text-white rounded-lg shadow-md hover:bg-indigo-700 m-3'>
+            <button className='bg-indigo-400 p-2 text-gray-100 rounded-lg shadow-md hover:bg-indigo-600 m-3'>
               Iniciar Sesión
             </button>
           </Link>
